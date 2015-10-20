5e7bb223f80c5730ddccff966fccbc065800499a
Guo Yubao <guoyubao71@163.com>
2015-10-20 23:22:19 +0800

    完成注册逻辑

--- ColorCloudZoneIOSProjectV1.0/ColorCloudZoneIOSProjectV1.0/RegisterViewController.m
+++ ColorCloudZoneIOSProjectV1.0/ColorCloudZoneIOSProjectV1.0/RegisterViewController.m
@@ -53,30 +53,32 @@ typedef void(^GBCompletionBlock)(BOOL success, NSError *error);
 
 - (IBAction)regisBtnAction:(id)sender {
     
-    [SVProgressHUD show];
+   
     NSString *phone = self.phoneTextField.text;
     NSString *pwd = self.passwordTextField.text;
     
     //做校验
     if (phone.length != 11) {
-        [SVProgressHUD dismiss];
+      
         [SVProgressHUD showErrorWithStatus:@"输入的手机号不正确"];
         return;
     }
     
     
     if (pwd.length <6) {
-        [SVProgressHUD dismiss];
+      
         [SVProgressHUD showErrorWithStatus:@"密码必须大于6为"];
         return;
     }
-    
+     [SVProgressHUD showWithStatus:@"正在注册..."];
     [self DoRegistBusinessWithUserName:phone Pwd:pwd
                         withCompletion:^(BOOL success, NSError *error) {
-                            [SVProgressHUD dismiss];
+                            
                             if (success) {
                                 //注册成功
-                                
+                                [self dismissViewControllerAnimated:YES completion:^{
+                                     [SVProgressHUD showInfoWithStatus:@"注册成功"];
+                                }];
                             }else {
                             //注册失败
                                 [SVProgressHUD showErrorWithStatus:@"注册失败，请重新尝试"];
